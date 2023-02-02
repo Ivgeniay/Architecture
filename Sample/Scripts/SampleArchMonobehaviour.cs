038ad467484b002ad0ca36ecd7244a904cbcccd9
Ivgeniay <45053991+Ivgeniay@users.noreply.github.com>
2023-02-02 19:40:29 +0400

    02.02.2023 19.41

--- Sample/Scripts/SampleArchMonobehaviour.cs
+++ Sample/Scripts/SampleArchMonobehaviour.cs
@@ -1,4 +1,5 @@
-﻿using Architecture.Root.ArchBehaviour;
+﻿using Architecture.ProjectResources.TwitchFacade;
+using Architecture.Root.ArchBehaviour;
 using NetWork.Twitch._Auth;
 using NetWork.Twitch._Client;
 using TwitchLib.Client;
@@ -8,9 +9,41 @@ namespace Assets._Project.Scripts.Sample.Scripts
 {
     internal class SampleArchMonobehaviour : MonoArchBehaviour
     {
+        TwitchAuthController twController;
+        TwitchClientController twitchClient;
+        TwitchFacadeController twitchFacade;
 
-        protected override void OnResourceEvent(OnResourceEventArgs onResourceEventArgs) {
+        protected override void OnResourceEvent(OnResourceEventArgs onResourceEventArgs)
+        {
             Debug.Log(onResourceEventArgs.Resource.ToString() + onResourceEventArgs.LoadingType);
+        }
+
+        protected override void OnSceneStart()
+        {
+            twController = Game.Instance.GetController<TwitchAuthController>();
+            twitchClient = Game.Instance.GetController<TwitchClientController>();
+            twitchFacade = Game.Instance.GetController<TwitchFacadeController>();
+
+            twController.OnTwitchConnectedEvent += OnTwitchConnectedHandler;
+        }
+
+        private void OnTwitchConnectedHandler() {
+            var rr = twController.GetSectet();
+            Debug.Log(rr);
+        }
+
+        private void Update()
+        {
+            if (Game.Instance.isLoaded) 
+            {
+                if (Input.GetKeyDown(KeyCode.Space)) 
+                {
+                    twitchFacade.Connect();
+                }                
+                if (Input.GetKeyDown(KeyCode.A)){
+                    twitchClient.SendMessage("Hello");
+                }
+            }
         }
     }
 }
